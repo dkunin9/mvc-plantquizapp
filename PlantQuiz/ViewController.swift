51a70952d560de8ca409abb777ea62c9fc8b5d2f
daniel <kuningram@gmail.com>
2021-02-10 20:17:46 +0300

    Initial Commit

---
+++ PlantQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantQuiz
+//
+//  Created by Daniel on 10.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

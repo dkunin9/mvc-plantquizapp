0cf0d622a2cc08a87c759c7fab07722f4ca362b1
Daniil <kuningram@gmail.com>
2021-02-19 13:20:15 +0300

    sign in, sign up added

--- PlantQuiz/Controllers/RegisterViewController.swift
+++ PlantQuiz/Controllers/RegisterViewController.swift
@@ -6,24 +6,32 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButtonPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e)
+                }
+                else {
+                    self.performSegue(withIdentifier: "RegisterToQuiz", sender: self)
+                }
+            }
+            
+        }
     }
-    */
-
+    
 }
